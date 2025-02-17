a9469c1aea1aa52332a90570115a5fb727fab8ba
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2025-02-18 01:24:04 +0800

    feat: add python-uv

--- flake.nix
+++ flake.nix
@@ -1,15 +1,25 @@
 {
   description = "Flake project templates";
 
-  outputs = {...}: {
-    templates = {
-      python-venv = {
-        path = ./python-venv;
-        description = "Python development template using venv";
-        welcomeText = ''
-          Creating a new Python project using venv
-        '';
+  outputs =
+    { self }:
+    {
+      templates = {
+        python-venv = {
+          path = ./python-venv;
+          description = "Python development template using venv";
+          welcomeText = ''
+            Creating a new Python project using venv
+          '';
+        };
+        python-uv = {
+          python = ./python-uv;
+          description = "Python development template using uv";
+          welcomeText = ''
+            # Getting started
+            - run `direnv allow`
+          '';
+        };
       };
     };
-  };
 }
