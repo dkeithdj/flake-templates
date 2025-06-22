46a584010ef78371a0abbbf6c92bc9a0dc83a726
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2025-06-22 20:24:54 +0800

    feat: add default envs

--- python-uv-simple/flake.nix
+++ python-uv-simple/flake.nix
@@ -8,12 +8,15 @@
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{ flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
 
       perSystem =
         { pkgs, system, ... }:
+        let
+          inherit (nixpkgs) lib;
+        in
         {
           devShells.default = pkgs.mkShell {
             name = "python-uv-simple";
@@ -21,12 +24,31 @@
             packages = with pkgs; [
               python313 # or whichever version you use
               uv # fast dependency resolver & installer
-            ];
+              gcc
+              zlib
 
+            ];
+            env =
+              {
+                # UV_PYTHON_DOWNLOADS = "never";
+                # UV_PYTHON = python.interpreter;
+                VIRTUAL_ENV = ".venv";
+              }
+              // lib.optionalAttrs pkgs.stdenv.isLinux {
+                # Python libraries often load native shared objects using dlopen(3).
+                # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+                LD_LIBRARY_PATH = lib.makeLibraryPath [
+                  pkgs.pythonManylinuxPackages.manylinux1
+                  pkgs.stdenv.cc.cc
+                  pkgs.zstd
+                  pkgs.zlib
+                ];
+                PATH = "$PWD/.venv/bin:$PATH";
+              };
             shellHook = ''
               echo "Welcome to the Python + uv dev shell!"
               echo "Running: uv venv && uv sync"
-              if [ ! -d .venv ]; then
+              if [ ! -d $VIRTUAL_ENV ]; then
                 uv venv
               fi
               uv sync --all-extras
